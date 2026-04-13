9851877103aa8bd38114c7beb10d6bceea32cef7
LekkerNakkie <97978497+LekkerNakkie@users.noreply.github.com>
2026-04-13 19:18:16 +0200

    Base

--- src/main/java/me/lekkernakkie/lekkeranimal/LekkerAnimal.java
+++ src/main/java/me/lekkernakkie/lekkeranimal/LekkerAnimal.java
@@ -1,17 +1,33 @@
 package me.lekkernakkie.lekkeranimal;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class LekkerAnimal {
-    static void main() {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        IO.println(String.format("Hello and welcome!"));
+import me.lekkernakkie.lekkeranimal.command.LekkerAnimalsCommand;
+import org.bukkit.plugin.java.JavaPlugin;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            IO.println("i = " + i);
-        }
+public class LekkerAnimal extends JavaPlugin {
+
+    private static LekkerAnimal instance;
+
+    @Override
+    public void onEnable() {
+        instance = this;
+
+        // Save default configs
+        saveDefaultConfig();
+        saveResource("animals.yml", false);
+        saveResource("Lang_NL.yml", false);
+
+        // Register command
+        getCommand("lekkeranimals").setExecutor(new LekkerAnimalsCommand());
+
+        getLogger().info("LekkerAnimal enabled!");
+    }
+
+    @Override
+    public void onDisable() {
+        getLogger().info("LekkerAnimal disabled!");
+    }
+
+    public static LekkerAnimal getInstance() {
+        return instance;
     }
 }
--- src/main/java/me/lekkernakkie/lekkeranimal/command/LekkerAnimalsCommand.java
+++ src/main/java/me/lekkernakkie/lekkeranimal/command/LekkerAnimalsCommand.java
@@ -1,4 +1,46 @@
 package me.lekkernakkie.lekkeranimal.command;
 
-public class LekkerAnimalsCommand {
+import me.lekkernakkie.lekkeranimal.LekkerAnimal;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+
+public class LekkerAnimalsCommand implements CommandExecutor {
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+
+        if (args.length == 0) {
+            sendHelp(sender);
+            return true;
+        }
+
+        if (args[0].equalsIgnoreCase("reload")) {
+            if (!sender.hasPermission("lekkeranimal.command.reload")) {
+                sender.sendMessage("§cNo permission.");
+                return true;
+            }
+
+            LekkerAnimal.getInstance().reloadConfig();
+            sender.sendMessage("§aConfig reloaded.");
+            return true;
+        }
+
+        if (args[0].equalsIgnoreCase("info")) {
+            sender.sendMessage("§bLekkerAnimal §7v1.0");
+            return true;
+        }
+
+        sendHelp(sender);
+        return true;
+    }
+
+    private void sendHelp(CommandSender sender) {
+        sender.sendMessage("§8--------------------------");
+        sender.sendMessage("§bLekkerAnimal Help");
+        sender.sendMessage("§e/lekkeranimals");
+        sender.sendMessage("§e/lekkeranimals reload");
+        sender.sendMessage("§e/lekkeranimals info");
+        sender.sendMessage("§8--------------------------");
+    }
 }
